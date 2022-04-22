9d998c8480de8ec4c2a6e77a18e38900550d2263
Farshid JahanManesh <samicancel2@gmail.com>
2022-04-23 00:24:29 +0430

    remove interface from imapfrom

---
+++ src/Core/BookStore.Core.Application/Contracts/IUserDataService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BookStore.Core.Application.Contracts
+{
+    public interface IUserDataService
+    {
+        public bool IsAuthenticated { get; set; }
+        public Guid UserId { get; set; }//todo: must set when add identity
+        public string CurrentURL { get; set; }
+    }
+}
